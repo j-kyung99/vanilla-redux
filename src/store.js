24a11698315dbfa01e61c2b6f349a11f726c1586
j-kyung99 <apfwnls14@naver.com>
2023-09-14 15:19:12 +0900

    #4.2 createReducer

--- src/store.js
+++ src/store.js
@@ -1,19 +1,29 @@
 import { legacy_createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 export const addToDo = createAction("ADD");
 export const deleteToDo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      return state.filter((toDo) => toDo.id !== parseInt(action.payload));
-    default:
-      return state;
-  }
-};
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter((toDo) => toDo.id !== parseInt(action.payload));
+//     default:
+//       return state;
+//   }
+// };
+
+const reducer = createReducer([], (builder) => {
+  builder
+    .addCase(addToDo, (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    })
+    .addCase(deleteToDo, (state, action) => {
+      return state.filter((toDo) => toDo.id !== action.payload);
+    });
+});
 
 const store = legacy_createStore(reducer);
 
